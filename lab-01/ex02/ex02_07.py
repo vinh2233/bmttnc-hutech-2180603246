f5b3a63886162ea32017a3f37a1e783c735e8f82
lequangvinh2180603246 <vinhquang064@gmail.com>
2024-04-01 11:12:39 +0700

    [add] lab-01 ex04 and .gitignore

--- lab-01/ex02/ex02_07.py
+++ lab-01/ex02/ex02_07.py
@@ -4,8 +4,8 @@ while True:
     line = input ()
     if line.lower () == 'done':
         break
-lines.append (line)
+    lines.append (line)
 # Chuyển các dòng thành chữ in hoa và in ra màn hình
 print ("\nCác dòng đã nhập sau khi chuyển thành chữ in hoa:")
 for line in lines:
-    print (line. upper ())
+    print (line.upper ())
